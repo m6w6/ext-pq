011422d5848488842bd2752f8dd6fdbb51fd884f
Michael Wallner <mike@php.net>
2015-07-30 20:51:12 +0200

    remove travis

--- travis/configure-postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-
-PQ_CREATE_DB_SQL="CREATE DATABASE $PG_TEST_DB_NAME;"
-PQ_DSN="postgres://$PG_TEST_DB_USER@localhost/$PG_TEST_DB_NAME"
-
-psql -c "$PQ_CREATE_DB_SQL" -U $PG_TEST_DB_USER
-echo "<?php const PQ_DSN = '$PQ_DSN';" > ./tests/_setup.inc
