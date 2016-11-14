74d8a9663d61da6b6a7021b7afaa1ebd416a471d
Michael Wallner <mike@php.net>
2016-11-14 17:47:14 +0100

    update travis

--- gen_travis_yml.php
+++ gen_travis_yml.php
@@ -17,7 +17,7 @@ env:
 
 $gen = include "./travis/pecl/gen-matrix.php";
 $env = $gen([
-	"PHP" => ["7.0", "master"],
+	"PHP" => ["7.0", "7.1", "master"],
 	"enable_debug",
 	"enable_maintainer_zts",
 	"enable_json",
